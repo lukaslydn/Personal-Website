774f5a6aef16d6ce37ed4effb96d534a5eab210d
Lukas <l.lydn32@gmail.com>
2026-02-10 14:47:51 +0000

    Update notfoundpage.jsx
    Fixed image bug

--- src/components/notfoundpage.jsx
+++ src/components/notfoundpage.jsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
-import Home from "../pages/Home";
+
 
 function NotFoundPage() {
     return ( 
         <div className="notfound-page">
             <Link to="/">
-            <img className="notfound-image" src="src/assets/notfound.png" />
+            <img className="notfound-image" src="../assets/notfound.png" />
             <p style={{color: 'white', textAlign: "center"}}>Page not found</p>
             </Link>
         </div>
