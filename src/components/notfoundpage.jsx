fa079f173094abed31714048438bd3100f5477ea
Lukas Lydon <lukaslydn@Lukass-MacBook-Pro.local>
2026-02-10 14:57:45 +0000

    Fixed 404 page and image

--- src/components/notfoundpage.jsx
+++ src/components/notfoundpage.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 
+import notfound from "../assets/notfound.png"
 
 function NotFoundPage() {
     return ( 
         <div className="notfound-page">
             <Link to="/">
-            <img className="notfound-image" src="../assets/notfound.png" />
+            <img className="notfound-image" src={notfound} />
             <p style={{color: 'white', textAlign: "center"}}>Page not found</p>
             </Link>
         </div>
